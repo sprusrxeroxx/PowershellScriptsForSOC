fa8f7d4f351242a4ceba5b8d4be95be2ce1bf328
Ntokozo L Vilakazi <90731554+sprusrxeroxx@users.noreply.github.com>
2025-02-17 20:43:51 +0200

    Update Get_Incident.ps1

--- Get_Incident.ps1
+++ Get_Incident.ps1
@@ -1,1 +1,3 @@
-Get-AzSentinelIncident -ResourceGroupName $resourceGroup -WorkspaceName $workspace | Where-Object {$_.Status -eq "New"} | Select-Object =ExpandProperty Name -First 1 
+Get-AzSentinelIncident -ResourceGroupName $resourceGroup -WorkspaceName $workspace | Where-Object {$_.Status -eq "New"} | Select-Object =ExpandProperty Name -First 1
+
+Get-AzSentinelIncident -ResourceGroupName $resourceGroup -WorkspaceName $workspace | Where-Object {$_.Status -eq "New"} | Select-Object -First 1 | Format-List Name, Status, Severity, Title
